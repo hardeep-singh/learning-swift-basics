dcb9fa1d8e87d2c6bc010a14fbb15e3eb313e7f1
Hardeep Singh <contact@hardeep-singh.com>
2023-09-27 17:25:42 +1300

    Nested types

---
+++ Codable/WhatIsCodable.playground/Pages/Nested types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+import Foundation
+
+struct Toy: Codable {
+  var name: String
+}
+
+struct Employee: Codable {
+  var name: String
+  var id: Int
+  var favoriteToy: Toy
+}
+
+let toy = Toy(name: "Teddy Bear")
+let employee = Employee(name: "John Appleseed", id: 7, favoriteToy: toy)
+
+let encoder = JSONEncoder()
+let decoder = JSONDecoder()
+
+let data = try encoder.encode(employee)
+let string = String(data: data, encoding: .utf8)!
+let sameEmployee = try decoder.decode(Employee.self, from: data)
